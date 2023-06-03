0019bf3f0f1624d427dbb83473e6c167fc210e76
omsurase <om.surase21@vit.edu>
2023-06-03 10:35:55 +0530

    recursive traversal of simple linked list

---
+++ linkedlist/recursivetraversalll.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+struct node 
+{
+    int data;
+    node *next;
+    node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printll (node *head)
+{
+    if (head != NULL)
+    {
+        cout<<head->data<<endl;
+        head = head->next;
+        printll(head);
+    }
+    else
+    {
+        return ;
+    }
+}
+
+int main()
+{
+    node *head = new node(10);
+    head -> next = new node (20);
+    head ->next->next = new node (30);
+
+    printll(head);
+    return 0;
+}
