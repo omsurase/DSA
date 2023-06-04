30956c30d50db774fb70b2f35ed59a1f91ee8d6f
omsurase <om.surase21@vit.edu>
2023-06-04 19:18:12 +0530

    delete the last node of doubly linked list

---
+++ linkedlist/deleteLastDLL.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+struct node 
+{
+    int data;
+    node* next;
+    node* prev;
+    node (int a)
+    {
+        data = a;
+        next = NULL;
+        prev = NULL;
+    }
+};
+
+node * deletelast(node *head)
+{
+    node *temp = head; 
+    while(temp->next!=NULL)
+    {
+        temp = temp->next;
+    }
+    node *temp2  = temp->prev;
+    temp2->next = NULL;
+    temp->prev = NULL;
+    delete temp;
+    return head;
+}
+int main()
+{
+    node *head = new node (10);
+    head ->next = new node(20);
+    head -> next ->next = new node(30);
+    head ->next -> next ->next = new node (40);
+    head ->next->prev = head;
+    head ->next->next->prev = head->next;
+    head ->next ->next->next->prev = head ->next->next;
+    node *head1 =head;
+    while (head!=NULL)
+    {
+        cout<<head->data<<" ";
+        head = head->next;
+    }
+    cout<<endl;
+    head1 = deletelast(head1);
+    while (head1!=NULL)
+    {
+        cout<<head1->data<<" ";
+        head1 = head1->next;
+    }
+    return 0;
+}
