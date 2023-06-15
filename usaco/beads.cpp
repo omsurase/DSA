70bd9bb33c0262298d5adb8d0e18029b663d0bd9
omsurase <om.surase21@vit.edu>
2023-06-16 01:48:35 +0530

    usaco broken necklace

--- usaco/beads.cpp
+++ usaco/beads.cpp
@@ -1,54 +1,51 @@
+/*
+ID: omsurase1
+TASK: beads
+LANG: C++                 
+*/
 #include<bits/stdc++.h>
 using namespace std;
 int main ()
-{   int n;
-    cin>>n;
+{
+    ofstream fout ("beads.out");
+    ifstream fin ("beads.in");
+    int n;
+    fin>>n;
     string s;
-    int e = -10e7;
-    cin>>s;
-    int i=n-1,j=0;
-    while(s[0]==s[i])
+    fin>>s;
+    s =" "+s+s+" ";
+    int dpl[800][2]; // 0 -> red 1->blue
+    int dpr[800][2];
+    dpl[0][0]=dpl[0][1]=0;
+    for ( int i = 1 ; i < n+n; i++)
     {
-        i--;
-        j++;
         
+        if(s[i]=='r') dpl[i][0] = dpl[i-1][0] + 1, dpl[i][1] = 0;
+        else if(s[i]=='b') dpl[i][1] = dpl[i-1][1] + 1, dpl[i][0] = 0;
+        else if(s[i]=='w') dpl[i][0] = dpl[i-1][0] + 1, dpl[i][1] = dpl[i-1][1] + 1;        
     }
-    while(s[i]=='w')
+    for ( int i = 1 ; i < n+n; i++)
     {
-        i--;
-        j++;
+        
+        if(s[i]=='r') dpl[i][0] = dpl[i-1][0] + 1, dpl[i][1] = 0;
+        else if(s[i]=='b') dpl[i][1] = dpl[i-1][1] + 1, dpl[i][0] = 0;
+        else if(s[i]=='w') dpl[i][0] = dpl[i-1][0] + 1, dpl[i][1] = dpl[i-1][1] + 1;        
     }
-    cout<<s[n-1]<<endl;
-    cout<<++i<<" "<<j<<endl;
-    string s1 = s.substr(i,j);
-    s = s1+s;
-    cout<<s<<endl;
-    i=0;
-    int b = 1;
-    while(i<s.size())
+    dpr[n+n+1][0]=dpr[n+n+1][1]=0;
+    for ( int i = n+n ; i > 0; i--)
     {
-        j = 1;
         
-        if(s[i]==s[i+1]&& i<s.size())
-        {
-           
-            ++j;
-        }
-        i++;
-        if(s[i]=='w' && i<s.size())
-        {
-            
-            ++j;
-        }
-        else
-        {
-            e = max(e,j);
-            j=1;
-        }
-        i++;
-        e = max(e,j);  //bwwbbrwrbrbrrbrbrwrwwrrwwbbbb
-        
+        if(s[i]=='r') dpr[i][0] = dpr[i+1][0] + 1, dpr[i][1] = 0;
+        else if(s[i]=='b') dpr[i][1] = dpr[i+1][1] + 1, dpr[i][0] = 0;
+        else if(s[i]=='w') dpr[i][0] = dpr[i+1][0] + 1, dpr[i][1] = dpr[i+1][1] + 1;        
     }
-    cout<<e<<endl;
+
+    int ans = 0;
+    for ( int i = 1 ; i < n+n; i++)
+    {
+        ans = max(ans, max(dpl[i][0],dpl[i][1] ) +max(dpr[i+1][0],dpr[i+1][1] ));
+    }
+    ans = min(ans,n);
+    fout<<ans<<endl;
     return 0;
 }
