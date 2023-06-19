2dba89b2875026bdaea919e47f18cf502550f029
omsurase <om.surase21@vit.edu>
2023-06-19 19:39:02 +0530

    879B

--- codeforces/879B.cpp
+++ codeforces/879B.cpp
@@ -6,12 +6,54 @@ int main ()
     cin>>t;
     while(t--)
     {
-        int a;
-        cin>>a;
-        while(a--)
+        string l,r;
+        cin>>l>>r;
+        if (l==r)
         {
-            cout<<a<<" ";
+            cout<<"0"<<endl;
+            continue;
         }
+        string p;
+        int len1 = l.length();
+        int len2 = r.length();
+        int maxLength = std::max(len1, len2);
+        string paddedNum1(maxLength - len1, '0');
+        string paddedNum2(maxLength - len2, '0');
+        string paddedNum1Final = paddedNum1 + l;
+        string paddedNum2Final = paddedNum2 + r;
+        
+       for (int i = 0; i < maxLength; i++) {
+        int digit1 = paddedNum1Final[i] - '0';
+        int digit2 = paddedNum2Final[i] - '0';
+        int difference = abs(digit1 - digit2);
+        p += to_string(difference);
+    }
+    
+        bool step = false;
+        long long ans = 0;
+        for (int i = 0; i < maxLength; i++)
+        {
+            if (step == false)
+            {
+                ans = ans + (p[i]-'0');
+                if (p[i]>'0')
+                {
+                    step = true;
+                }
+                else step = false;
+            }
+            else if (step == true)
+            {
+                ans = ans + 9;
+                if (p[i]>'0')
+                {
+                    step = true;
+                }
+                else step = false;
+            }
+        }
+
+        cout<<ans<<endl;
     }
     return 0;
 }
